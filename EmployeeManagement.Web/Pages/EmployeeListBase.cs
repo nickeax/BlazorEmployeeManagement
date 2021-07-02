f9c123ae87e13a66be1cfebcc00440b106faef49
Nick Fletcher <nickeax@gmail.com>
2021-07-02 13:55:59 +1000

    Add waiting behaviour

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -11,14 +11,14 @@ namespace EmployeeManagement.Web.Pages
     public class EmployeeListBase : ComponentBase
     {
         public IEnumerable<Employee> Employees { get; set; }
-        protected override Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync()
         {
-            LoadEmployees();
-            return base.OnInitializedAsync();
+            await Task.Run(LoadEmployees);
         }
 
         private void LoadEmployees()
         {
+            System.Threading.Thread.Sleep(2300);
             Employee e1 = new Employee
             {
                 Employeeid = 1,
