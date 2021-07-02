fad51fec91cc84158a2f5f4398ad95512b51a128
Nick Fletcher <nickeax@gmail.com>
2021-07-02 17:22:48 +1000

    Begin API, implement GET.

---
+++ EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeManagement.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await _employeeRepository.GetEmployees());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { value = e.Message });
+            }
+        }
+    }
+}
